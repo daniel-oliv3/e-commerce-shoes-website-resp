3e26a17cc67011d078f2f6957c6110379531f5ea
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-08 19:35:02 -0300

    Slide btn prev and next

--- js/script.js
+++ js/script.js
@@ -12,3 +12,23 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
+
+/*Prev e Next slide btn*/
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+
+
+
