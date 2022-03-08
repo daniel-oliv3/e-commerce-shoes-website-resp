ba9dc2aac24f1bb22ab7e48ca5ea6bf5fbf80d21
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-08 15:17:14 -0300

    Menu toggle

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*Menu-bar Toggle tbn navbar*/
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
