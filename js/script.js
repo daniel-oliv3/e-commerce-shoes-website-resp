38ceb628ac3d3f69f7961326eb2cf45c6a52fdd9
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-03-10 11:30:01 -0300

    Featured javascript alterando as imagens

--- js/script.js
+++ js/script.js
@@ -29,6 +29,24 @@ function prev(){
     slides[index].classList.add('active');
 }
 
+/*featured - alterando as imgs*/
+document.querySelectorAll('.featured-image-1').forEach(image_1 => {
+    image_1.addEventListener('click', () => {
+        var src = image_1.getAttribute('src');
+        document.querySelector('.big-image-1').src = src;
+    });
+});
 
+document.querySelectorAll('.featured-image-2').forEach(image_2 => {
+    image_2.addEventListener('click', () => {
+        var src = image_2.getAttribute('src');
+        document.querySelector('.big-image-2').src = src;
+    });
+});
 
-
+document.querySelectorAll('.featured-image-3').forEach(image_3 => {
+    image_3.addEventListener('click', () => {
+        var src = image_3.getAttribute('src');
+        document.querySelector('.big-image-3').src = src;
+    });
+});
